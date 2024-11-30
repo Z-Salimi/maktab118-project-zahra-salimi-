e76d47d3ae39fa0c969e32d6bf463b3a77791199
Z-Salimi <zahra.2002.salimi@gmail.com>
2024-11-30 22:08:19 +0330

    add user

---
+++ app/users/[id]/page.tsx
@@ -0,0 +1,9 @@
+
+const UsersByIdPage: React.FC =()=>{
+    return(
+        <section>
+            <h1>UsersByIdPage</h1>
+        </section>
+    )
+}
+export default UsersByIdPage;
---
+++ app/users/page.tsx
@@ -0,0 +1,9 @@
+
+const UsersPage: React.FC =()=>{
+    return(
+        <section>
+            <h1>UsersPage</h1>
+        </section>
+    )
+}
+export default UsersPage;
