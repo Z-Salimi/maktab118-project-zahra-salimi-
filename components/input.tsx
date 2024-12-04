9db18a094ea79b05deac18ddd2e255455ccc6093
Z-Salimi <zahra.2002.salimi@gmail.com>
2024-12-05 00:16:20 +0330

    update component input

--- components/input.tsx
+++ components/input.tsx
@@ -8,7 +8,7 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 export const Input: React.FC<InputProps> = ({ label, error, ...props }) => {
   return (
     <div  className="w-full flex flex-col justify-center gap-2">
-      <label className="text-slate-700 capitalize">{label}</label>
+      <label className="text-slate-700">{label}</label>
       <input  className="border border-slate-300 bg-slate-50 rounded-lg md:max-w-[40vw] w-full px-3 py-1.5 text-sm shadow-md" {...props} />
       {error && <p className="text-red-700 text-sm font-medium">{error}</p>}
     </div>
