c1c07f408d89dc9f9f90067756e7af0ee31d02fd
Z-Salimi <zahra.2002.salimi@gmail.com>
2024-12-05 00:16:54 +0330

    update navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,21 +5,19 @@ import { Button } from "./button";
 
 export const Navbar: React.FC = () => {
   return (
-    <header className="px-6 py-3 flex justify-around items-center gap-4 w-full">
+    <header className="px-6 py-3 flex justify-around items-center gap-4 w-full bg-gray-200">
+      <img src="digizarger.png" alt="Logo" className="w-64" />
+      <div className="w-[30vw]">
+        <Input type="search" placeholder="جستجو..." />
+      </div>
       <div className="flex justify-center items-center gap-4">
         <Link href={"/auth/login"}>
-          <button className="bg-gray-100 px-6 py-1 rounded-xl text-gray-900 font-semibold border-r-2 border-b-2 border-gray-100 shadow-[4px_4px_5px_lightgray] transition-all ease-in-out duration-500 hover:shadow-[-4px_-4px_5px_lightgray]">
-            ورود
-          </button>
+          <Button text="ورود" />
         </Link>
         <Link href={"/admin/auth/login"}>
           <Button text="پنل مدیریت" />
         </Link>
       </div>
-      <div className="w-[30vw]">
-        <Input type="search" placeholder="Search..." />
-      </div>
-      <img src="digizarger.png" alt="Logo" className="w-64" />
     </header>
   );
 };
