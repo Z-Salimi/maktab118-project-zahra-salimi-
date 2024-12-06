78e43462a4cce2dfd105bc734972774758cd3a4e
Z-Salimi <zahra.2002.salimi@gmail.com>
2024-12-06 16:19:45 +0330

    add page productManage

---
+++ app/admin/management/manageProduct/page.tsx
@@ -0,0 +1,13 @@
+
+import { AllProductManage } from "@/container/AllProductManage";
+import React from "react";
+
+const ManageProduct: React.FC = () => {
+  
+  return (
+    <section className=" w-full h-screen p-10 bg-backManage bg-no-repeat bg-cover">
+      <AllProductManage />
+    </section>
+  );
+};
+export default ManageProduct;
