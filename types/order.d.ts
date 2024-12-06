7b6f01cda58efe60dd9c64b40f39f76630981aa8
Z-Salimi <zahra.2002.salimi@gmail.com>
2024-12-06 21:46:04 +0330

    add types

---
+++ types/order.d.ts
@@ -0,0 +1,21 @@
+interface IOrder {
+    _id: string;
+    user: string;
+    products: Array<{ product: string; count: number }>;
+    totalPrice: number;
+    deliveryDate: string | null;
+    deliveryStatus: boolean;
+    createdAt: string;
+    updatedAt: string;
+  }
+  
+  interface IResOrders {
+    status: string;
+    page: number;
+    per_page: number;
+    total: number;
+    total_pages: number;
+    data: {
+      orders: IOrder[];
+    };
+  }
---
+++ types/user.d.ts
@@ -0,0 +1,16 @@
+interface IUser {
+  _id: string;
+  firstname: string;
+  lastname: string;
+  username: string;
+  phoneNumber: string;
+  address: string;
+  role: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface IResUsers {
+  total: number;
+  data: IUser[];
+}
