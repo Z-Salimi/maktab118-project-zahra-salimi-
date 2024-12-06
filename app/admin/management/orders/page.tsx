e153ef44f0f3b3bea3cf886a64191a86838928f1
Z-Salimi <zahra.2002.salimi@gmail.com>
2024-12-06 21:46:27 +0330

    add order page

--- app/admin/management/orders/page.tsx
+++ app/admin/management/orders/page.tsx
@@ -1,7 +1,9 @@
+import { OrderManage } from "@/container/OrderManage";
+
 const OrdersManagement:React.FC =()=>{
     return(
         <section className=" w-full h-screen p-10 bg-backOrder bg-no-repeat bg-cover">
-      
+      <OrderManage />
     </section>
     )
 }
