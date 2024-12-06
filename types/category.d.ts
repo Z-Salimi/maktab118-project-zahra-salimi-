9c9177554744aab379f4045d0529319e2de2ccb1
Z-Salimi <zahra.2002.salimi@gmail.com>
2024-12-06 16:23:09 +0330

    add type

---
+++ types/category.d.ts
@@ -0,0 +1,11 @@
+interface ICategory{
+    category: {
+            _id: string;
+            name: string
+            icon: string;
+            slugname: string;
+            createdAt: string;
+            updatedAt: string;
+            __v: number;
+        }
+}
---
+++ types/subCategory.d.ts
@@ -0,0 +1,11 @@
+interface ISubCategory {
+  subcategory: {
+    _id: string;
+    category: ICategory;
+    name: string;
+    createdAt: string;
+    updatedAt: string;
+    slugname: string;
+    __v: number;
+  };
+}
