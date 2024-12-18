a55ab106770a7620e1b9050649756e1b7e895f70
Z-Salimi <zahra.2002.salimi@gmail.com>
2024-12-19 02:18:17 +0330

    add pages for categories

---
+++ app/categories/necklace/page.tsx
@@ -0,0 +1,10 @@
+import { CategoryFetch } from "@/components/categoryFetch";
+
+const NecklacePage: React.FC =()=>{
+    return(
+        <section>
+            <CategoryFetch id="674f5e9bd4ce8b2f706d402b" />
+        </section>
+    )
+}
+export default NecklacePage;
---
+++ app/categories/set/page.tsx
@@ -0,0 +1,10 @@
+import { CategoryFetch } from "@/components/categoryFetch";
+
+const SetPage: React.FC =()=>{
+    return(
+        <section>
+            <CategoryFetch id="674f60ced4ce8b2f706d402e" />
+        </section>
+    )
+}
+export default SetPage;
