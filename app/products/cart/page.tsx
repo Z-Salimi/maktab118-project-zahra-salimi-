7d0d6332b10e39f1e83b3ef21178624271de453d
Z-Salimi <zahra.2002.salimi@gmail.com>
2024-12-20 00:08:25 +0330

    update ui and add products

--- app/products/cart/page.tsx
+++ app/products/cart/page.tsx
@@ -1,8 +1,11 @@
-const CartPage: React.FC =()=>{
-    return(
-        <section>
-            <h1>CartPage</h1>
-        </section>
-    )
-}
+import { CartPageContainer } from "@/container/CartPageContainer";
+
+const CartPage: React.FC = () => {
+  return (
+    <section className="pt-14 px-4">
+      <h1 className="text-2xl text-gray-700 font-bold mb-8">سبد خرید</h1>
+      <CartPageContainer />
+    </section>
+  );
+};
 export default CartPage;
