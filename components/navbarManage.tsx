02a7b95f2b0f243854e0dffa74072efc8d09789b
Z-Salimi <zahra.2002.salimi@gmail.com>
2024-12-26 00:09:28 +0330

    update navbars for logout

--- components/navbarManage.tsx
+++ components/navbarManage.tsx
@@ -1,8 +1,10 @@
-'use client'
+"use client";
+import { logoutRequest } from "@/apis/services/auth.service";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { MdExitToApp } from "react-icons/md";
 
 export const NavbarManagement: React.FC = () => {
   const path = usePathname();
@@ -15,6 +17,10 @@ export const NavbarManagement: React.FC = () => {
   const isActive = (pathname: string) => {
     return path.includes(pathname) ? "bg-slate-100" : "bg-slate-200";
   };
+  const handleLogout = () => {
+    logoutRequest();
+    window.location.href = "/"; // Redirect to home after logout
+  };
 
   return (
     <section>
@@ -25,36 +31,68 @@ export const NavbarManagement: React.FC = () => {
             {isOpen ? <FaTimes size={30} /> : <FaBars size={30} />}
           </button>
         </div>
-        <div className={`fixed top-0 left-0 right-0 bg-gray-200 z-20 md:relative md:flex md:flex-col md:items-start md:mt-4 transition-transform transform ${isOpen ? "translate-y-0" : "-translate-y-full"} md:translate-y-0 duration-500`}>
-          <img src="/digizarger.png" alt="Logo" className="w-64 mt-4 hidden md:block" />
+        <div
+          className={`fixed top-0 left-0 right-0 bg-gray-200 z-20 md:relative md:flex md:flex-col md:items-start md:mt-4 transition-transform transform ${
+            isOpen ? "translate-y-0" : "-translate-y-full"
+          } md:translate-y-0 duration-500`}
+        >
+          <img
+            src="/digizarger.png"
+            alt="Logo"
+            className="w-64 mt-4 hidden md:block"
+          />
           <div className={`shadow-md mb-3 w-full md:w-[25vw]`}>
             <Link href={"/"}>
-              <button className={`mt-10 rounded-md w-full p-6 transition-all ease-in-out duration-500 hover:bg-slate-100`}>
+              <button
+                className={`mt-10 rounded-md w-full p-6 transition-all ease-in-out duration-500 hover:bg-slate-100`}
+              >
                 صفحه اصلی
               </button>
             </Link>
           </div>
           <div className={`shadow-md mb-3 w-full md:w-[25vw]`}>
             <Link href={"/admin/management/manageProduct"}>
-              <button className={`${isActive("/admin/management/manageProduct")} rounded-md w-full p-6 transition-all ease-in-out duration-500 hover:bg-slate-100`}>
+              <button
+                className={`${isActive(
+                  "/admin/management/manageProduct"
+                )} rounded-md w-full p-6 transition-all ease-in-out duration-500 hover:bg-slate-100`}
+              >
                 مدیریت محصولات
               </button>
             </Link>
           </div>
           <div className={`shadow-md mb-3 w-full md:w-[25vw]`}>
             <Link href={"/admin/management/manageStock"}>
-              <button className={`${isActive("/admin/management/manageStock")} rounded-md w-full p-6 transition-all ease-in-out duration-500 hover:bg-slate-100`}>
+              <button
+                className={`${isActive(
+                  "/admin/management/manageStock"
+                )} rounded-md w-full p-6 transition-all ease-in-out duration-500 hover:bg-slate-100`}
+              >
                 مدیریت موجودی و قیمت
               </button>
             </Link>
           </div>
           <div className={`shadow-md mb-3 w-full md:w-[25vw]`}>
             <Link href={"/admin/management/orders"}>
-              <button className={`${isActive("/admin/management/orders")} rounded-md w-full p-6 transition-all ease-in-out duration-500 hover:bg-slate-100`}>
+              <button
+                className={`${isActive(
+                  "/admin/management/orders"
+                )} rounded-md w-full p-6 transition-all ease-in-out duration-500 hover:bg-slate-100`}
+              >
                 مدیریت سفارش ها
               </button>
             </Link>
           </div>
+          <div className={`shadow-md mb-3 w-full md:w-[25vw]`}>
+
+          <button
+            className="rounded-md w-full p-6 transition-all ease-in-out duration-500 hover:bg-slate-100 flex justify-center items-center gap-4"
+            onClick={handleLogout}
+          >
+            <MdExitToApp className="size-6 text-gray-600" />خروج
+          </button>
+          </div>
+
         </div>
       </div>
     </section>
