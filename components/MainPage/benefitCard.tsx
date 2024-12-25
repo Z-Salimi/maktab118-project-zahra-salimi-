1f665592d0f743172685d2d5939cc53638464bbe
Z-Salimi <zahra.2002.salimi@gmail.com>
2024-12-26 00:07:11 +0330

    update and edit some components

---
+++ components/MainPage/benefitCard.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface IBeneficCard{
+    text: string;
+    src: string;
+}
+export const BenefitCard: React.FC<IBeneficCard> = ({text,src}) => {
+  return (
+    <div className="flex flex-col justify-center items-center gap-4">
+      <div className="flex justify-center items-center rounded-full size-50 p-2 lg:size-40 border-white border-4 bg-slate-200 shadow-[4px_4px_5px_lightgray] transition-all ease-in-out duration-500 hover:shadow-[-4px_-4px_5px_lightgray]">
+        <img src={src} alt="" />
+      </div>
+      <h4 className="text-gray-500 text-lg font-semibold">{text}</h4>
+    </div>
+  );
+};
