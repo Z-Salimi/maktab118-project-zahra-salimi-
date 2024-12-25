ee0a59177d1ad3966ad4dffbd4505952e5e497e1
Z-Salimi <zahra.2002.salimi@gmail.com>
2024-12-26 00:07:59 +0330

    update guard

---
+++ validation/signup.validation.ts
@@ -0,0 +1,10 @@
+// import { z } from "zod";
+
+// export const SignupSchema = z.object({
+//   firstname: z.string().min(1, "نام الزامی است"),
+//   lastname: z.string().min(1, "نام خانوادگی الزامی است"),
+//   username: z.string().min(1, "نام کاربری الزامی است"),
+//   password: z.string().min(8, "رمز عبور باید حداقل ۸ کاراکتر باشد"),
+//   phoneNumber: z.string().regex(/^\d{11}$/, "شماره تلفن نامعتبر است"),
+//   address: z.string().min(1, "آدرس الزامی است"),
+// });
