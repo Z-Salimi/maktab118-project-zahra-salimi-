b8b644082c74185a841c45c728995acf54aae3a0
Z-Salimi <zahra.2002.salimi@gmail.com>
2025-01-09 02:00:31 +0330

    add subCategories

---
+++ app/categories/bracelet/[id]/page.tsx
@@ -0,0 +1,11 @@
+
+import { SubCategoryFetch } from "@/components/subCategoryFetch";
+
+const BraceletSubCategoryPage: React.FC =()=>{
+    return(
+        <section>
+            <SubCategoryFetch />
+        </section>
+    )
+}
+export default BraceletSubCategoryPage;
---
+++ app/categories/necklace/[id]/page.tsx
@@ -0,0 +1,11 @@
+
+import { SubCategoryFetch } from "@/components/subCategoryFetch";
+
+const NecklaceSubCategoryPage: React.FC =()=>{
+    return(
+        <section>
+            <SubCategoryFetch />
+        </section>
+    )
+}
+export default NecklaceSubCategoryPage;
